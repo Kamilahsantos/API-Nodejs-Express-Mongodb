75e10150633acb79713a3f16d1fee127df724d34
Kamila Santos <kahsantos89@gmail.com>
2024-08-28 07:13:23 -0300

    projeto final do curso

---
+++ src/config/dbConnect.js
@@ -0,0 +1,11 @@
+
+
+import mongoose from "mongoose";
+
+async function conectaNaDatabase(){
+    mongoose.connect(process.env.DB_CONNECTION_STRING)
+ return mongoose.connection;
+}
+
+
+export default conectaNaDatabase;
